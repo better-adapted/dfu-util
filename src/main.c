f207e7d9b90833b02e8f272b42b0fd88a623589b
better-adapted <better-adapted@users.noreply.github.com>
2026-03-25 15:51:11 +0000

    pushed a comment change

--- src/main.c
+++ src/main.c
@@ -207,6 +207,7 @@ static void print_version(void)
 	printf(PACKAGE_STRING "\n\n");
 	printf("Copyright 2005-2009 Weston Schmidt, Harald Welte and OpenMoko Inc.\n"
 	       "Copyright 2010-2021 Tormod Volden and Stefan Schmidt\n"
+		   "Modified Verison by SperrinLogic.com\n"
 	       "This program is Free Software and has ABSOLUTELY NO WARRANTY\n"
 	       "Please report bugs to " PACKAGE_BUGREPORT "\n\n");
 }
